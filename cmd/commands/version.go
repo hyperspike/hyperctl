5a0ae12c148d8bf6d760cce172ba3b769cb9c0c9
Dan Molik <dan@danmolik.com>
2020-10-15 11:52:16 -0400

    print go verion on version command

--- cmd/commands/version.go
+++ cmd/commands/version.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"runtime"
 	"github.com/spf13/cobra"
 	"hyperspike.io/hyperctl"
 )
@@ -15,7 +16,7 @@ func Version() *cobra.Command {
 		Use: "version",
 		Short: "Hyperctl Version",
 		Run: func(c *cobra.Command, args []string) {
-			fmt.Printf("Hyperctl Version: %s\n  Kubernetes Version: %s\n", hyperctl.Version, hyperctl.KubeVersion)
+			fmt.Printf("Hyperctl Version: %s\n  Go Version: %s\n  Kubernetes Version: %s\n", hyperctl.Version, runtime.Version(), hyperctl.KubeVersion)
 		},
 	}
 
