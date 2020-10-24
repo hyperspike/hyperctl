2e8296eae4f9a63012b025785f95b8233fae4849
Dan Molik <dan@danmolik.com>
2020-10-24 09:37:52 -0400

    output to tmp file

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"os"
+	"io"
 	log "github.com/sirupsen/logrus"
 	"hyperspike.io/hyperctl/cmd/commands"
 )
 func init() {
+	logFile, err := os.Create("/tmp/hyperspike.log")
+	if err != nil {
+		panic(err)
+	}
+	// defer logFile.Close()
+	mw := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(mw)
 	log.SetFormatter(&log.TextFormatter{
 		DisableColors: false,
 		FullTimestamp: true,
