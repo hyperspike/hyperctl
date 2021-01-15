c05dd80628558d22ee37e415f98548ac156bf713
Dan Molik <dan@danmolik.com>
2021-01-15 14:36:35 -0500

    more cilium debugging [CI SKIP]

--- version.go
+++ version.go
@@ -2,5 +2,5 @@ package hyperctl
 
 var Version string
 var KubeVersion string = "v1.18.15" // v1.19.3
-var CiliumVersion string = "v1.8.5" // v1.9.0
+var CiliumVersion string = "v1.8.6" // v1.9.0
 var AlpineVersion string = "3.12.3"
