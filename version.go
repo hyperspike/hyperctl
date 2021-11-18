2cb39d798cdafd610569172174edc58fbb17e757
Dan Molik <dan@danmolik.com>
2021-11-18 13:51:30 -0500

    It builds!

--- version.go
+++ version.go
@@ -1,6 +1,6 @@
 package hyperctl
 
 var Version string
-var KubeVersion string = "v1.18.17" // v1.19.3
-var CiliumVersion string = "v1.8.8" // v1.9.0
-var AlpineVersion string = "3.12.3" // 3.13.0
+var KubeVersion string = "v1.22.3"
+var CiliumVersion string = "v1.10.5"
+var AlpineVersion string = "3.14.3"
