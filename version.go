ad8eb6ed23aa0e0433820bafc2bb944084fc49ae
Dan Molik <dan@danmolik.com>
2022-01-23 09:15:04 -0500

    Update versions and add place holders for components

--- version.go
+++ version.go
@@ -1,6 +1,12 @@
 package hyperctl
 
 var Version string
-var KubeVersion string = "v1.18.17" // v1.19.3
-var CiliumVersion string = "v1.8.8" // v1.9.0
-var AlpineVersion string = "3.12.3" // 3.13.0
+var KubeVersion string = "v1.23.2"
+var CiliumVersion string = "v1.11.1"
+var AlpineVersion string = "3.15.0"
+var IRSAVerson string = ""
+var CCMVersion string = ""
+var EBSVersion string = ""
+var NodeTermVersion string = ""
+var KMSVersion string = ""
+var ClusterAutoscalerVersion = "
